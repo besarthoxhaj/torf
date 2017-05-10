03b61ce376947aa0050dd57cb8b7cd211f9a0527
Besart Shyti <BesartS@canneslions.com>
2017-05-10 08:41:53 +0100

    [housekeeping] fix indentation

--- index.js
+++ index.js
@@ -1,89 +1,99 @@
 'use strict';
 
-
-
 module.exports = {
-	ok:    ok,
-	email: checkEmail,
-	type:  isType,
-	empty: isEmpty
+  ok: ok,
+  email: checkEmail,
+  type: isType,
+  empty: isEmpty
 };
 
-
-
-function isEmpty (obj){
-
-	return (Object.getOwnPropertyNames(obj).length === 0);
+/**
+ * [isEmpty description]
+ * @param  {[type]}  obj [description]
+ * @return {Boolean}     [description]
+ */
+function isEmpty (obj) {
+  return (Object.getOwnPropertyNames(obj).length === 0);
 }
 
-
-
-function ok (arg){
-
-	switch (Object.prototype.toString.call(arg)){
-		case '[object Array]': 
-			return arg.length < 1 ? false : ok(arg[0]) ? true : false;
-		case '[object Object]':
-			return isEmpty(arg) ? false : true;
-		case '[object Null]':
-			return false;
-		case '[object Undefined]':
-			return false;
-		case '[object Number]':
-			return isFinite(arg);
-		case '[object String]':
-			return true;
-		case '[object Boolean]':
-			return arg;
-		case '[object Function]':
-			return ok(arg());
-		default:
-			return true;
-	}
+/**
+ * [ok description]
+ * @param  {[type]} arg [description]
+ * @return {[type]}     [description]
+ */
+function ok (arg) {
+  switch (Object.prototype.toString.call(arg)) {
+    case '[object Array]':
+      return arg.length < 1 ? false : ok(arg[0]) ? true : false;
+    case '[object Object]':
+      return isEmpty(arg) ? false : true;
+    case '[object Null]':
+      return false;
+    case '[object Undefined]':
+      return false;
+    case '[object Number]':
+      return isFinite(arg);
+    case '[object String]':
+      return true;
+    case '[object Boolean]':
+      return arg;
+    case '[object Function]':
+      return ok(arg());
+    default:
+      return true;
+  }
 }
 
-
-
+/**
+ * [getClassName description]
+ * @param  {[type]} arg [description]
+ * @return {[type]}     [description]
+ */
 function getClassName (arg) {
-	return Object.prototype.toString.call(arg).match(/\s[a-zA-Z]+/)[0].trim().toLowerCase();
+  return Object.prototype.toString.call(arg).match(/\s[a-zA-Z]+/)[0].trim().toLowerCase();
 }
 
-
-
-function isType (arg, type){
-
-	return (function test (arg, type){
-
-		if(getClassName(type) === 'array'){
-			if(getClassName(arg) === 'object' && type[0] === 'object'){
-				return true;
-			}else if(getClassName(arg) === type[0] && isFinite(arg)){
-				return true;
-			}else if(getClassName(type[0]) === 'undefined'){
-				return false;
-			}else{
-				type.shift();
-				return test(arg, type);
-			}
-		}else{
-			if(getClassName(arg) === 'number' && type === 'number'){
-				return isFinite(arg);
-			}else{
-				return getClassName(arg) === type;
-			}
-		}
-	})(arg, type);
+/**
+ * [isType description]
+ * @param  {[type]}  arg  [description]
+ * @param  {[type]}  type [description]
+ * @return {Boolean}      [description]
+ */
+function isType (arg, type) {
+  return (function test(arg, type) {
+    if(getClassName(type) === 'array'){
+      if(getClassName(arg) === 'object' && type[0] === 'object'){
+        return true;
+      }else if(getClassName(arg) === type[0] && isFinite(arg)){
+        return true;
+      }else if(getClassName(type[0]) === 'undefined'){
+        return false;
+      }else{
+        type.shift();
+        return test(arg, type);
+      }
+    }else{
+      if(getClassName(arg) === 'number' && type === 'number'){
+        return isFinite(arg);
+      }else{
+        return getClassName(arg) === type;
+      }
+    }
+  })(arg, type);
 }
 
-
-
+/**
+ * [checkEmail description]
+ * @param  {[type]} email  [description]
+ * @param  {[type]} regexp [description]
+ * @return {[type]}        [description]
+ */
 function checkEmail (email, regexp){
-
-	if(ok(email)){
-		var myRegexp = isType(regexp, 'regexp') ? regexp : /\S+@\S+\.\S+/;
-		var result   = myRegexp.test(email);
-		return result;
-	}else{
-		return false;
-	}
+  if(ok(email)) {
+    var myRegexp = isType(regexp, 'regexp') ? regexp : /\S+@\S+\.\S+/;
+    var result = myRegexp.test(email);
+    return result;
+  }else{
+    return false;
+  }
 }
--- test/is.test.js
+++ test/is.test.js
@@ -1,175 +1,148 @@
 'use strict';
 
-
-var root   = __dirname + '/../';
+var root = __dirname + '/../';
 var expect = require('chai').expect;
-var is     = require(root + 'index');
-
+var is = require(root + 'index');
 
 var shouldPassOk = [
-	[1],
-	['bes'],
-	{'bes':'foo'},
-	[{'bes':'foo'}],
-	new Date(),
-	true,
-	function(){return true;},
-	function(){return ['bes'];},
-	function(){return {'bes':'foo'};},
-	function(){return [{'bes':'foo'}];},
-	function(){return function(){return true;};}
+  [1],
+  ['bes'],
+  {'bes':'foo'},
+  [{'bes':'foo'}],
+  new Date(),
+  true,
+  function(){return true;},
+  function(){return ['bes'];},
+  function(){return {'bes':'foo'};},
+  function(){return [{'bes':'foo'}];},
+  function(){return function(){return true;};}
 ];
-
 
 var shouldPassType = [
-	[/\S+@\S+\.\S+/, 'regexp'],
-	[null, 'null'],
-	[undefined, 'undefined'],
-	['string', 'string'],
-	[['bes'], 'array'],
-	[{'bes':'foo'}, 'object'],
-	[new Date(), 'date'],
-	[true, 'boolean'],
-	[function(){return true;}, 'function'],
-	[25, 'number'],
-	[25, ['number','string']],
-	[{}, ['number','string','object']],
-	[[], ['number','string','object','array']],
-	[new Date(), ['number','string','object','array','randomString','date']]
+  [/\S+@\S+\.\S+/, 'regexp'],
+  [null, 'null'],
+  [undefined, 'undefined'],
+  ['string', 'string'],
+  [['bes'], 'array'],
+  [{'bes':'foo'}, 'object'],
+  [new Date(), 'date'],
+  [true, 'boolean'],
+  [function(){return true;}, 'function'],
+  [25, 'number'],
+  [25, ['number','string']],
+  [{}, ['number','string','object']],
+  [[], ['number','string','object','array']],
+  [new Date(), ['number','string','object','array','randomString','date']]
 ];
-
 
 var shouldFailOk = [
-	[],
-	{},
-	[[]],
-	[{}],
-	[[{}]],
-	NaN,
-	null,
-	Infinity,
-	undefined,
-	[NaN],
-	[null],
-	[false],
-	[Infinity],
-	[undefined],
-	function(){},
-	function(){return NaN;},
-	function(){return null;},
-	function(){return false;},
-	function(){return Infinity;},
-	function(){return undefined;},
-	function(){return [undefined];},
-	function(){return function(){return false;};}
+  [],
+  {},
+  [[]],
+  [{}],
+  [[{}]],
+  NaN,
+  null,
+  Infinity,
+  undefined,
+  [NaN],
+  [null],
+  [false],
+  [Infinity],
+  [undefined],
+  function(){},
+  function(){return NaN;},
+  function(){return null;},
+  function(){return false;},
+  function(){return Infinity;},
+  function(){return undefined;},
+  function(){return [undefined];},
+  function(){return function(){return false;};}
 ];
-
 
 var shouldFailType = [
-	[null, 'undefined'],
-	[new Date(), 'number'],
-	[undefined, 'array'],
-	['string', 'object'],
-	[['bes'], 'string'],
-	[{'bes':'foo'}, 'array'],
-	[new Date(), 'boolean'],
-	[true, 'date'],
-	[function(){return true;}, 'Math'],
-	[Infinity, 'number'],
-	[2, 'randomString'],
-	[{}, 'randomString'],
-	[Infinity, ['number','string']],
-	[{}, ['number','string','array','date','null','undefined']],
-	[null, ['number','string','date']],
-	[null, ['number','string','date']],
-	[NaN, ['number','string','date']],
-	[new Date(), ['string','object','array','null','number']],
-	[new Date(), ['string','object','array','null','randomString']]
+  [null, 'undefined'],
+  [new Date(), 'number'],
+  [undefined, 'array'],
+  ['string', 'object'],
+  [['bes'], 'string'],
+  [{'bes':'foo'}, 'array'],
+  [new Date(), 'boolean'],
+  [true, 'date'],
+  [function(){return true;}, 'Math'],
+  [Infinity, 'number'],
+  [2, 'randomString'],
+  [{}, 'randomString'],
+  [Infinity, ['number','string']],
+  [{}, ['number','string','array','date','null','undefined']],
+  [null, ['number','string','date']],
+  [null, ['number','string','date']],
+  [NaN, ['number','string','date']],
+  [new Date(), ['string','object','array','null','number']],
+  [new Date(), ['string','object','array','null','randomString']]
 ];
-
 
 describe('#is.ok()', function (){
 
-	shouldPassOk.forEach(function (elm){
-
-		it('"' + JSON.stringify(elm) + '" should return TRUE', function (){
-
-			expect(is.ok(elm)).to.equal(true);
-		});
-	});
+  shouldPassOk.forEach(function (elm){
+    it('"' + JSON.stringify(elm) + '" should return TRUE', function (){
+      expect(is.ok(elm)).to.equal(true);
+    });
+  });
 
 
-	shouldFailOk.forEach(function (elm){
-
-		it('"' + JSON.stringify(elm) + '" should return FALSE', function (){
-
-			expect(is.ok(elm)).to.equal(false);
-		});
-	});
+  shouldFailOk.forEach(function (elm){
+    it('"' + JSON.stringify(elm) + '" should return FALSE', function (){
+      expect(is.ok(elm)).to.equal(false);
+    });
+  });
 });
-
-
 
 describe('#is.email()', function (){
 
-	it('should return TRUE for foo@bar.com', function (){
+  it('should return TRUE for foo@bar.com', function (){
+    expect(is.email('foo@bar.com')).to.equal(true);
+  });
 
-		expect(is.email('foo@bar.com')).to.equal(true);
-	});
+  it('should return TRUE for foo@bar.com', function (){
+    expect(is.email('foo@bar.com', new RegExp(/[\s\S]/))).to.equal(true);
+  });
 
+  it('should return FALSE for null', function (){
+    expect(is.email(null)).to.equal(false);
+  });
 
-	it('should return TRUE for foo@bar.com', function (){
-
-		expect(is.email('foo@bar.com', new RegExp(/[\s\S]/))).to.equal(true);
-	});
-
-
-	it('should return FALSE for null', function (){
-
-		expect(is.email(null)).to.equal(false);
-	});
-
-
-	it('should return FALSE for foo.zoo@bar', function (){
-
-		expect(is.email('foo.zoo@bar')).to.equal(false);
-	});
+  it('should return FALSE for foo.zoo@bar', function (){
+    expect(is.email('foo.zoo@bar')).to.equal(false);
+  });
 
 
-	it('should return FALSE for null', function (){
-
-		expect(is.email(null)).to.equal(false);
-	});
+  it('should return FALSE for null', function (){
+    expect(is.email(null)).to.equal(false);
+  });
 });
 
 
 
 describe('#is.empty()', function (){
 
-	it('should return TRUE for "{}"', function (){
-
-		expect(is.empty({})).to.equal(true);
-	});
+  it('should return TRUE for "{}"', function (){
+    expect(is.empty({})).to.equal(true);
+  });
 });
 
 
 describe('#is.type()', function (){
 
-	shouldPassType.forEach(function (elm){
+  shouldPassType.forEach(function (elm){
+    it('"' + JSON.stringify(elm) + '" should return TRUE', function (){
+      expect(is.type.apply(null, elm)).to.equal(true);
+    });
+  });
 
-		it('"' + JSON.stringify(elm) + '" should return TRUE', function (){
-
-			expect(is.type.apply(null, elm)).to.equal(true);
-		});
-	});
-
-
-
-	shouldFailType.forEach(function (elm){
-
-		it('"' + JSON.stringify(elm) + '" should return FALSE', function (){
-
-			expect(is.type.apply(null, elm)).to.equal(false);
-		});
-	});
+  shouldFailType.forEach(function (elm){
+    it('"' + JSON.stringify(elm) + '" should return FALSE', function (){
+      expect(is.type.apply(null, elm)).to.equal(false);
+    });
+  });
 });
